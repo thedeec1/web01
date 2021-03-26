cdbb61421c5cc124e6ac8aa2d36836f41b64eb6c
Darren Cotton <Darren.Cotton@tunstall.com>
2021-03-26 15:50:03 +0000

    front end state 1

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import { ConnectedDashboard } from './Dashboard';
+import { Router, Route } from 'react-router-dom';
+import { history } from '../store/history'
+import { ConnectedNavigation } from './Navigation';
+import { ConnectTaskDetail } from './TaskDetail'
 
 export const Main = () => (
-    <Provider store={store}>
-        <div>
-            {/* Dashboard goes here */}
-            <ConnectedDashboard/>
-        </div>
-    </Provider>
-
+    <Router history={history}>
+        <Provider store={store}>
+            <div>
+                < ConnectedNavigation />
+                {/* Dashboard goes here */}
+                {/* <ConnectedDashboard/> */}
+                <Route exect path="/dashboard" render={()=>(<ConnectedDashboard/>)} />
+                <Route exact path="/task/:id" render={({match})=>(<ConnectTaskDetail match={match} />)}/>
+            </div>
+        </Provider>
+    </Router>
 );
